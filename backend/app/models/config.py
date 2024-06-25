ae2ec75dc2fc1252cf8dc8f9a6af6a4f51c471c7
HashLain <Code_Wrench@proton.me>
2024-06-25 16:05:41 -0500

    New branch for testing DB, delete later

--- backend/app/models/config.py
+++ backend/app/models/config.py
@@ -2,13 +2,20 @@ class Config:
     # Google Search API
     GOOGLE_API_KEY = 'AIzaSyDa83ukRR4dlqykb_5NmxRm3o-BKj6_zkY'
     GOOGLE_CX = '674dd5606f7914a34'
+    #MySQL: User Database
+    
+    USER_DB_HOST = '194.104.156.210'
+    USER_DB_PORT = '3306'
+    USER_DB_USER = 'u30529_i8EvvPQCAB'
+    USER_DB_PASS = '1BrMMkcomapI9zdW=aeuBMMl'
+    USER_DB_NAME = 's30529_userDB'
 
     # MySQL: User Database
-    USER_DB_HOST = 'localhost'
-    USER_DB_PORT = '3306'
-    USER_DB_USER = 'bassholes'
-    USER_DB_PASS = 'password'
-    USER_DB_NAME = 'userDB'
+    #USER_DB_HOST = 'localhost'
+    #USER_DB_PORT = '3306'
+    #USER_DB_USER = 'bassholes'
+    #USER_DB_PASS = 'password'
+    #USER_DB_NAME = 'userDB'
 
     # MySQL: Knowledgebase Database
     #KNOW_DB_HOST = 'localhost'
